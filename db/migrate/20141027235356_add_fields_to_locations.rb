b24492397baaecae1a5615fad95ad49ce6cdbfa2
Al Snow <jasnow@hotmail.com>
2017-02-23 19:14:50 -0500

    Added 4.2 to migration scripts

--- db/migrate/20141027235356_add_fields_to_locations.rb
+++ db/migrate/20141027235356_add_fields_to_locations.rb
@@ -1,4 +1,4 @@
-class AddFieldsToLocations < ActiveRecord::Migration
+class AddFieldsToLocations < ActiveRecord::Migration[4.2]
   def change
     add_column :locations, :ebt, :boolean
     add_column :locations, :county, :string
