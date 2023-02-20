14348040bec85da7329e967d32e51f7acaab854b
Al Snow <jasnow@hotmail.com>
2023-02-20 17:14:01 -0500

    Rebuild db; added 2 GH-A and .ruby-version files

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014_10_27_235356) do
-
+ActiveRecord::Schema[7.0].define(version: 2014_10_27_235356) do
   create_table "locations", force: :cascade do |t|
     t.float "latitude"
     t.float "longitude"
